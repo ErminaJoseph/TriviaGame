2a4790e8ec225f1197fa9d55c3de6842f70b960e
Ermina_Joseph <ermina.joseph@gmail.com>
2019-11-13 14:44:45 -0500

    added coding of results page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -43,7 +43,7 @@ $(document).ready(function() {
             answer: "St. Cloud, MN",
             choices: ["St. Cloud, MN", "St. Paul, MN", "St. Michael, MN", "St. Joseph, MN"],
         }, {
-            question: "Why does Robin hate going to the mall?",
+            question: "What did Ted say to Robin on their first date?",
             answer: "I love you",
             choices: ["Marry me", "This was a mistake", "I love you", "I just want to be casual"]
         }]
@@ -63,6 +63,19 @@ $(document).ready(function() {
         } else {
             incorrectAnswer();
         }
+
+        nextQuestionPrompt = setTimeout(function() {
+            timer = 31;
+            $("#question").html(questionAndAnswers[index].question);
+            $("#choice-one").html(questionAndAnswers[index].choices[0]);
+            $("#choice-two").html(questionAndAnswers[index].choices[1]);
+            $("#choice-three").html(questionAndAnswers[index].choices[2]);
+            $("#choice-four").html(questionAndAnswers[index].choices[3]);
+            console.log(questionAndAnswers[index].choices[0])
+            console.log(questionAndAnswers[index].choices[1])
+            console.log(questionAndAnswers[index].choices[2])
+            console.log(questionAndAnswers[index].choices[3])
+        }, 3000);
     });
 
     $("#choice-two").on("click", function() {
@@ -75,28 +88,69 @@ $(document).ready(function() {
         } else {
             incorrectAnswer();
         }
+
+        nextQuestionPrompt = setTimeout(function() {
+            timer = 31;
+            $("#question").html(questionAndAnswers[index].question);
+            $("#choice-one").html(questionAndAnswers[index].choices[0]);
+            $("#choice-two").html(questionAndAnswers[index].choices[1]);
+            $("#choice-three").html(questionAndAnswers[index].choices[2]);
+            $("#choice-four").html(questionAndAnswers[index].choices[3]);
+            console.log(questionAndAnswers[index].choices[0])
+            console.log(questionAndAnswers[index].choices[1])
+            console.log(questionAndAnswers[index].choices[2])
+            console.log(questionAndAnswers[index].choices[3])
+        }, 3000);
     });
 
     $("#choice-three").on("click", function() {
         console.log("Yes");
         userChoice = $(this).text();
         console.log(userChoice);
-        if (userChoice === questionAndAnswers[index].answer[index]) {
+        console.log(questionAndAnswers[index].answer);
+        if (userChoice === questionAndAnswers[index].answer) {
             correctAnswer();
         } else {
             incorrectAnswer();
         }
+
+        nextQuestionPrompt = setTimeout(function() {
+            timer = 31;
+            $("#question").html(questionAndAnswers[index].question);
+            $("#choice-one").html(questionAndAnswers[index].choices[0]);
+            $("#choice-two").html(questionAndAnswers[index].choices[1]);
+            $("#choice-three").html(questionAndAnswers[index].choices[2]);
+            $("#choice-four").html(questionAndAnswers[index].choices[3]);
+            console.log(questionAndAnswers[index].choices[0])
+            console.log(questionAndAnswers[index].choices[1])
+            console.log(questionAndAnswers[index].choices[2])
+            console.log(questionAndAnswers[index].choices[3])
+        }, 2000);
     });
 
     $("#choice-four").on("click", function() {
         console.log("Yes");
         userChoice = $(this).text();
         console.log(userChoice);
-        if (userChoice === questionAndAnswers[index].answer[index]) {
+        console.log(questionAndAnswers[index].answer)
+        if (userChoice === questionAndAnswers[index].answer) {
             correctAnswer();
         } else {
             incorrectAnswer();
         }
+
+        nextQuestionPrompt = setTimeout(function() {
+            timer = 31;
+            $("#question").html(questionAndAnswers[index].question);
+            $("#choice-one").html(questionAndAnswers[index].choices[0]);
+            $("#choice-two").html(questionAndAnswers[index].choices[1]);
+            $("#choice-three").html(questionAndAnswers[index].choices[2]);
+            $("#choice-four").html(questionAndAnswers[index].choices[3]);
+            console.log(questionAndAnswers[index].choices[0])
+            console.log(questionAndAnswers[index].choices[1])
+            console.log(questionAndAnswers[index].choices[2])
+            console.log(questionAndAnswers[index].choices[3])
+        }, 2000);
     });
 
     function decrement () {
@@ -104,9 +158,41 @@ $(document).ready(function() {
         $("#display").html("Time Remaining: " + timer);
 
         if (timer === 0) {
+            $("#display").empty();
+            $("#question").empty();
+            $("#choice-one").html("Time's up! the correct answer is: " + questionAndAnswers[index].answer);
+            $("#choice-two").empty();
+            $("#choice-three").empty();
+            $("#choice-four").empty();
+            console.log(questionAndAnswers[index].answer);
+            index ++;
+            incorrectAnswer++;
+            
 
+            nextQuestionPrompt = setTimeout(function() {
+                timer = 31;
+                $("#question").html(questionAndAnswers[index].question);
+                $("#choice-one").html(questionAndAnswers[index].choices[0]);
+                $("#choice-two").html(questionAndAnswers[index].choices[1]);
+                $("#choice-three").html(questionAndAnswers[index].choices[2]);
+                $("#choice-four").html(questionAndAnswers[index].choices[3]);
+                console.log(questionAndAnswers[index].choices[0])
+                console.log(questionAndAnswers[index].choices[1])
+                console.log(questionAndAnswers[index].choices[2])
+                console.log(questionAndAnswers[index].choices[3])
+            }, 2000);
+        }
+
+        if (index === 10) {
+            $("#display").html("Let's see how you did!");
+            $("#question").empty();
+            $("#choice-one").html("Number of Correct Answers: " + correctAnswers);
+            $("#choice-two").html("Number of Incorrect Answers: " + incorrectAnswers);
+            $("#choice-three").empty(" ");
+            $("#choice-four").empty(" ");
         }
     }
+
 
     function startGame () {
         $("#display").empty()
@@ -120,15 +206,30 @@ $(document).ready(function() {
 
     function correctAnswer () {
         $("#display").empty();
-        $("#choices").html("You are correct!");
+        $("#question").empty();
+        $("#choice-one").html("You are correct!");
+        $("#choice-two").html(" ");
+        $("#choice-three").html(" ");
+        $("#choice-four").html(" ");
         correctAnswers++;
+        console.log(correctAnswers);
         index++;
+        console.log(index);
     }
 
     function incorrectAnswer () {
-        $("#dislay").empty();
-        $("#choices").html("Sorry, that is incorrect!");
+        $("#dipslay").empty();
+        $("#question").empty();
+        $("#choice-one").html("Whoops!  The correct answer is: " + questionAndAnswers[index].answer);
+        $("#choice-two").html(" ");
+        $("#choice-three").html(" ");
+        $("#choice-four").html(" ");
         incorrectAnswers++;
+        console.log(incorrectAnswers);
         index++;
+        console.log(index);
+    
     }
+
+
 });
